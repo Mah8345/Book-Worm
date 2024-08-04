3be5977e26533118bd00025a75b7c48f9ceb6d7a
Mahyar Esmaeeli <mah8345es@gmail.com>
2024-08-05 00:01:31 +0330

    feat:added a custom exception to throw when entity not found

---
+++ BookWorm/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace BookWorm.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public EntityNotFoundException(string entityName, object key)
+            : base($"{entityName} with key:{key} was not found")
+        {
+
+        }
+    }
+}
