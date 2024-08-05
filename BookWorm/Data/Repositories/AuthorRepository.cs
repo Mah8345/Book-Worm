9980a177046f351db4e58a7c702ec500c3cd58a7
Mahyar Esmaeeli <mah8345es@gmail.com>
2024-08-06 00:57:48 +0330

    feat: added author repositories

---
+++ BookWorm/Data/Repositories/AuthorRepository.cs
@@ -0,0 +1,34 @@
+﻿using BookWorm.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BookWorm.Data.Repositories
+{
+    public class AuthorRepository(ApplicationDbContext context) : GenericRepository<Author>(context), IAuthorRepository
+    {
+        public async Task<Author?> GetAuthorWithPhotoAsync(int id)
+        {
+            return await Context.Authors
+                .Include(a => a.Photo)
+                .FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public Task<Author?> GetAuthorWithPhotoAndBooksAsync(int id)
+        {
+            return Context.Authors
+                .Include(a => a.WrittenBooks)
+                .Include(a => a.Photo)
+                .FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task<IEnumerable<Author>> SearchAuthorsByNameAsync(string key)
+        {
+            key = key.ToUpper();
+            var authors = await Context.Authors
+                .Include(a => a.Photo)
+                .Where(a => a.NormalizedName.Contains(key))
+                .ToListAsync();
+            return authors.IsNullOrEmpty() ? new List<Author>() : authors;
+        }
+    }
+}
---
+++ BookWorm/Data/Repositories/IAuthorRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookWorm.Models;
+
+namespace BookWorm.Data.Repositories
+{
+    public interface IAuthorRepository
+    {
+        Task<Author?> GetAuthorWithPhotoAsync(int id);
+        Task<Author?> GetAuthorWithPhotoAndBooksAsync(int id);
+        Task<IEnumerable<Author>> SearchAuthorsByNameAsync(string key);
+    }
+}
