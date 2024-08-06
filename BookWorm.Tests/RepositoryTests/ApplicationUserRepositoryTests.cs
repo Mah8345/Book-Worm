f3c7ca6b88970a68700a54f1d37c3bab9ee529f4
Mahyar Esmaeeli <mah8345es@gmail.com>
2024-08-06 18:43:50 +0330

    test:added unit tests for ApplicationUserRepository

---
+++ BookWorm.Tests/RepositoryTests/ApplicationUserRepositoryTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookWorm.Data;
+using BookWorm.Data.Repositories;
+using BookWorm.Models;
+using BookWorm.Tests.TestHelpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace BookWorm.Tests.RepositoryTests
+{
+    public class ApplicationUserRepositoryTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly ApplicationDbContext _context;
+        private readonly ApplicationUserRepository _userRepository;
+
+        public ApplicationUserRepositoryTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "BookWormTest")
+                .LogTo(Console.WriteLine,LogLevel.Information)
+                .Options;
+            _context = new ApplicationDbContext(options);
+            _userRepository = new ApplicationUserRepository(_context);
+        }
+
+        
+        
+
+        [Fact]
+        public async Task GetUserByIdAsync_GetUsers()
+        {
+            //Arrange
+            var id = Guid.NewGuid().ToString();
+            var user = TestHelper.AddUser(id,_context);
+
+            //Act
+            var result = await _userRepository.GetUserByIdAsync(id);
+
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(id, result.Id);
+            Assert.Equal(user.Bio,result.Bio);
+
+            Assert.NotEmpty(result.FavoriteBooks);
+            for (int i = 1; i < 11; i++)
+            {
+                Assert.Contains(result.FavoriteBooks, a => a.Id == i);
+            }
+
+            Assert.NotEmpty(result.FavoriteAuthors);
+            for (int i = 1; i < 11; i++)
+            {
+                Assert.Contains(result.FavoriteAuthors, a => a.Id == i);
+            }
+
+            Assert.NotEmpty(result.FavoriteGenres);
+            for (int i = 1; i < 11; i++)
+            {
+                Assert.Contains(result.FavoriteGenres, g => g.Id == i);
+            }
+
+        }
+    }
+}
---
+++ BookWorm.Tests/TestHelpers/TestHelper.cs
@@ -0,0 +1,85 @@
+﻿using BookWorm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookWorm.Data;
+
+namespace BookWorm.Tests.TestHelpers
+{
+    public static class TestHelper
+    {
+        public static List<Book> GenerateRandomBooks(int count)
+        {
+            var books = new List<Book>();
+            for (int i = 0; i < count; i++)
+            {
+                books.Add(new Book()
+                {
+                    Title = $"Book_{i}",
+                    NormalizedTitle = $"BOOK_{i}",
+                    Introduction = $"introduction_{i}",
+                    Summary = $"summary_{i}",
+                    PagesNumber = 100 + (i % 3) * 20
+                });
+            }
+
+            return books;
+        }
+
+        public static List<Genre> GenerateRandomGenres(int count)
+        {
+            var genres = new List<Genre>();
+            for (int i = 0; i < count; i++)
+            {
+                genres.Add(new Genre()
+                {
+                    Name = $"Genre_{i}",
+                    NormalizedName = $"GENRE_{i}",
+                    Description = $"description_{i}"
+                });
+            }
+
+            return genres;
+        }
+
+        public static List<Author> GenerateRandomAuthors(int count)
+        {
+            var authors = new List<Author>();
+            for (int i = 0; i < count; i++)
+            {
+                authors.Add(new Author()
+                {
+                    Name = $"Author{i}",
+                    NormalizedName = $"AUTHOR{i}",
+                    About = $"about Author_{i}"
+                });
+            }
+
+            return authors;
+        }
+
+        public static ApplicationUser AddUser(string id,ApplicationDbContext context)
+        {
+            var user = new ApplicationUser()
+            {
+                Id = id,
+                FirstName = "FirstName",
+                LastName = "LastName",
+                AccessFailedCount = 10,
+                Bio = "Hey, I'm very interested in reading!",
+                Comments = new List<Comment>(),
+                DateOfBirth = DateTime.Now,
+                Email = "example@ex.com",
+                EmailConfirmed = false,
+                FavoriteAuthors = GenerateRandomAuthors(10),
+                FavoriteBooks = GenerateRandomBooks(10),
+                FavoriteGenres = GenerateRandomGenres(10)
+            };
+            context.ApplicationUsers.Add(user);
+            context.SaveChanges();
+            return user;
+        }
+    }
+}
