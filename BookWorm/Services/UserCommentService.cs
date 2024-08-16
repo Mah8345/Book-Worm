d674c68edaaf3ab28bdcf04c616fb411911b2e5b
Mahyar Esmaeeli <mah8345es@gmail.com>
2024-08-17 01:47:45 +0330

    feat:added a service to enable user to add and remove comments to and from a book

---
+++ BookWorm/Services/UserCommentService.cs
@@ -0,0 +1,31 @@
+﻿using BookWorm.Data.UnitOfWork;
+using BookWorm.Models;
+
+namespace BookWorm.Services
+{
+    public class UserCommentService(IUnitOfWork unitOfWork)
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        public async Task AddComment(string userId, int bookId, Comment comment)
+        {
+            var user = await EntityServiceUtils.GetUser(userId, user => user.Comments, unitOfWork);
+            var book = await EntityServiceUtils.GetBook(bookId, book => book.Comments, unitOfWork);
+            comment.CommentedBy = user;
+            book.Comments ??= [];
+            await _unitOfWork.SaveChangesAsync();
+            await EntityServiceUtils.AddToList(comment, book.Comments, unitOfWork);
+        }
+
+
+        public async Task<bool> RemoveComment(string userId, int bookId, int commentId)
+        {
+            var user = await EntityServiceUtils.GetUser(userId, user => user.Comments, unitOfWork);
+            var book = await EntityServiceUtils.GetBook(bookId, book => book.Comments, unitOfWork);
+            var comment = await EntityServiceUtils.GetComment(commentId, unitOfWork);
+            comment.CommentedBy = user;
+            var result = await EntityServiceUtils.RemoveFromList(comment, book.Comments, _unitOfWork);
+            return result;
+        }
+    }
+}
