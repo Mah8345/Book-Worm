8b4a53875cece4ca21db4b10acf14cfe69601fd9
Mahyar Esmaeeli <mah8345es@gmail.com>
2024-08-17 01:47:03 +0330

    feat:added a service to enable user to add and remove their favorite authors

---
+++ BookWorm/Services/UserAuthorService.cs
@@ -0,0 +1,26 @@
+﻿using BookWorm.Data.UnitOfWork;
+
+namespace BookWorm.Services
+{
+    public class UserAuthorService(IUnitOfWork unitOfWork)
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+
+        public async Task AddAuthorToFavoritesAsync(string userId, int authorId)
+        {
+            var user = await EntityServiceUtils.GetUser(userId, user => user.FavoriteAuthors, unitOfWork);
+            var author = await EntityServiceUtils.GetAuthor(authorId, unitOfWork);
+            await EntityServiceUtils.AddToList(author, user.FavoriteAuthors, unitOfWork);
+        }
+
+
+        public async Task<bool> RemoveAuthorFromFavorites(string userId, int authorId)
+        {
+            var user = await EntityServiceUtils.GetUser(userId, user => user.FavoriteAuthors, unitOfWork);
+            var author = await EntityServiceUtils.GetAuthor(authorId, unitOfWork);
+            var result = await EntityServiceUtils.RemoveFromList(author, user.FavoriteAuthors, unitOfWork);
+            return result;
+        }
+    }
+}
