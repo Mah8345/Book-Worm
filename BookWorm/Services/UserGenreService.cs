52eefca00e4a4bd1b1ef17111e573338f8f85f6d
Mahyar Esmaeeli <mah8345es@gmail.com>
2024-08-17 01:48:16 +0330

    feat:added a service to enable user to add and remove their favorite genres

---
+++ BookWorm/Services/UserGenreService.cs
@@ -0,0 +1,27 @@
+﻿using BookWorm.Data.UnitOfWork;
+
+namespace BookWorm.Services
+{
+    public class UserGenreService(IUnitOfWork unitOfWork)
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+
+        public async Task AddGenreToFavoritesAsync(string userId, int genreId)
+        {
+            var user = await EntityServiceUtils.GetUser(userId, user => user.FavoriteGenres, _unitOfWork);
+            user.FavoriteAuthors ??= [];
+            var genre = await EntityServiceUtils.GetGenre(genreId, _unitOfWork);
+            await EntityServiceUtils.AddToList(genre, user.FavoriteGenres, _unitOfWork);
+        }
+
+
+        public async Task<bool> RemoveGenreFromFavorites(string userId, int genreId)
+        {
+            var user = await EntityServiceUtils.GetUser(userId, user => user.FavoriteGenres, _unitOfWork);
+            var genre = await EntityServiceUtils.GetGenre(genreId, _unitOfWork);
+            var result = await EntityServiceUtils.RemoveFromList(genre, user.FavoriteGenres, _unitOfWork);
+            return result;
+        }
+    }
+}
