c8755d21e2cd6423ccf435ac71ffd286aea0c4db
Mahyar Esmaeeli <mah8345es@gmail.com>
2024-08-17 21:57:26 +0330

    chore:used property instead of primary contructor parameter

--- BookWorm/Services/UserCommentService.cs
+++ BookWorm/Services/UserCommentService.cs
@@ -9,20 +9,18 @@ namespace BookWorm.Services
 
         public async Task AddComment(string userId, int bookId, Comment comment)
         {
-            var user = await EntityServiceUtils.GetUser(userId, user => user.Comments, unitOfWork);
-            var book = await EntityServiceUtils.GetBook(bookId, book => book.Comments, unitOfWork);
+            var user = await EntityServiceUtils.GetUser(userId, user => user.Comments, _unitOfWork);
+            var book = await EntityServiceUtils.GetBook(bookId, book => book.Comments, _unitOfWork);
             comment.CommentedBy = user;
-            book.Comments ??= [];
-            await _unitOfWork.SaveChangesAsync();
-            await EntityServiceUtils.AddToList(comment, book.Comments, unitOfWork);
+            await EntityServiceUtils.AddToList(comment, book.Comments, _unitOfWork);
         }
 
 
         public async Task<bool> RemoveComment(string userId, int bookId, int commentId)
         {
-            var user = await EntityServiceUtils.GetUser(userId, user => user.Comments, unitOfWork);
-            var book = await EntityServiceUtils.GetBook(bookId, book => book.Comments, unitOfWork);
-            var comment = await EntityServiceUtils.GetComment(commentId, unitOfWork);
+            var user = await EntityServiceUtils.GetUser(userId, user => user.Comments, _unitOfWork);
+            var book = await EntityServiceUtils.GetBook(bookId, book => book.Comments, _unitOfWork);
+            var comment = await EntityServiceUtils.GetComment(commentId, _unitOfWork);
             comment.CommentedBy = user;
             var result = await EntityServiceUtils.RemoveFromList(comment, book.Comments, _unitOfWork);
             return result;
