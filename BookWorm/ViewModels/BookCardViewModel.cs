0dc40571ed0535a20f5d737f3dfa89d6c994269e
Mahyar Esmaeeli <mah8345es@gmail.com>
2024-08-24 01:20:16 +0330

    feat:added a view to display information of books in home page

---
+++ BookWorm/ViewModels/BookCardViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BookWorm.ViewModels
+{
+    public class BookCardViewModel
+    {
+        private double _rating;
+        public string? CoverImagePath { get; set; }
+        public string Title { get; set; }
+
+        public string Rating
+        {
+            get => _rating.ToString("F1");
+            set => _rating = double.Parse(value);
+        }
+    }
+}
