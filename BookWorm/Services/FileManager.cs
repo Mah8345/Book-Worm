84c9f02fa74e68393264a25967b9e917ea47d413
Mahyar Esmaeeli <mah8345es@gmail.com>
2024-09-13 16:28:00 +0330

    chore:added a static class for file management

---
+++ BookWorm/Services/FileManager.cs
@@ -0,0 +1,19 @@
+﻿using BookWorm.Models;
+
+namespace BookWorm.Services
+{
+    public static class FileManager
+    {
+        public static async Task SavePhotoAsync(Guid photoId, IFormFile photoFile)
+        {
+            var imageFormat = Path.GetExtension(photoFile.FileName);
+            var fileName = $"{photoId}{imageFormat}";
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", fileName);
+            Console.WriteLine(path);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await photoFile.CopyToAsync(stream);
+            }
+        }
+    }
+}
